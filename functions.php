0479a80e23f557563887a53935d3dc52beb62515
Aaron Parks <aaparks@csumb.edu>
2018-09-21 20:06:37 +0000

    added functions and index .php files

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+//setup
+function setup(){
+    return true;
+}
+
+//bid
+// function bid(){}
+
+//play
+function play(){
+    return true;
+}
+
+//score
+function score(){
+    return true;
+}
+
+//display
+function display(){
+    return true;
+}
+
+?>
