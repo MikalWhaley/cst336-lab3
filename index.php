8ccd0d3f63b726eb6a6a8e9b8ef175f2243c6291
Aaron Parks <aaparks@csumb.edu>
2018-09-25 19:19:39 +0000

    updated functionsA and index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> SilverJack </title>
+        <style>
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <?php
+                include 'functionsA.php';
+                play();
+            ?>
+
+        </div>
+        
+    </body>
+</html>
