7a5d07c08cc10724a40cd22e8fc1035811c21c52
Aaron Parks <aaparks@csumb.edu>
2018-09-26 09:35:20 +0000

    completed lab3

--- functionsA.php
+++ functionsA.php
@@ -2,14 +2,15 @@
 
 //setup
 function setup(){
-    $deck1 = generateDeck();
-    shuffle($deck1['cards']);
+    $deck = generateDeck();
+    shuffle($deck['cards']);
     $players = getHand($deck);
+    return $players;
     
 }
 
 function generateDeck(){
-    $suits = array('C', 'S', 'H', 'D');
+    $suits = array('clubs', 'spades', 'hearts', 'diamonds');
     $face = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13');
     $deck = array("numCards"=>52, "cards"=>array());
     
@@ -27,16 +28,16 @@ function generateDeck(){
 
 //play
 function play(){
-    setup();
-    score();
-    display();
+    $players = setup();
+    $players = score($players);
+    display($players);
     return true;
 }
 
 //score
 function score($players){
     for($x = 0; $x < 4; $x++){
-        $values[$x] = $players[$x]["value"];
+        $values[$x] = $players['player'][$x]["value"];
     }
     rsort($values);
     $winners = 0;
@@ -53,47 +54,69 @@ function score($players){
         }
     }
     for($x = 0; $x < 4; $x++){
-        if($players[$x]["value"] != $winners_score){
-            $totalScore = $totalScore + $players["value"][$x];
+        if($players['player'][$x]["value"] != $winners_score){
+            $totalScore = $totalScore + $players['player'][$x]["value"];
+            $players['player'][$x]["winner"] = false;
         }
     }
-    for($x = 0; $x < $winners; $x++){
-        if($players[$x]["value"] == $winners_score){
-            $players[$x]["score"] = $totalScore;
+    for($x = 0; $x < 4; $x++){
+        if($players['player'][$x]["value"] == $winners_score){
+            $players['player'][$x]["score"] = $totalScore;
+            $players['player'][$x]["winner"] = true;
         }
     }
-    return true;
+    return $players;
 }
 
 
 //display
-function display(){
+function display($players){
+    echo "<div id='output'>";
     for($x = 0; $x < 4; $x++){
-        echo "<img id='player$x' src='players/$x.png' alt='$x' title='Player $x' width='70' >";
-        for($y = 0; $y < sizeof($players["cards"]); $y++){
-            $source = "cards/" . $players[$x]["cards"]["suit"] . "/" . $players[$x]["cards"]["face"] . ".png";
-            echo "<img id='player$x_cards' src='$source' alt='card$x' title = 'Card $x' width='70' >";
+        $name = $players['player'][$x]['name'];
+        echo "<figure class='item'>";
+        echo "<img id='player' src='players/$x.jpg' alt='$x' title='Player $x' width='70' >";
+        echo "<figcaption class='caption'>$name</figcaption>";
+        echo "</figure>";
+        for($y = 0; $y < count($players['player'][$x]["hand"]); $y++){
+            $source = "cards/" . $players['player'][$x]["hand"][$y]["suit"] . "/" . $players['player'][$x]["hand"][$y]["face"] . ".png";
+            echo "<img id='cards' src='$source' alt='card$y' title = 'Card $y' width='70' >";
+        }
+        $score_to_echo = $players['player'][$x]['value'];
+        echo "<h3 id='score'> Score: $score_to_echo </h3>";
+        echo "<br><br>";
+    }
+    for($x = 0; $x < 4; $x++){
+        if($players['player'][$x]["winner"] == true){
+            $winner = $players['player'][$x]['name'];
+            $points = $players['player'][$x]['score'];
+            echo "<h2> $winner wins $points points!!! </h2>";
+            echo "<br><br>";
         }
     }
-    for($x = 0; $x < 4; $x++){
-        echo $players['name'][$x] . ": ";
-    }
-    for($x = 0; $x < 4; $x++){
-        echo "Score" . $players['value'][$x];
-    }
-    
-    
+    echo "</div";
     return true;
 }
 //get hand
 function getHand($deck){
     $players = array('numplayers'=>4, 'player' =>array());
+    $players['player'][0]['name'] = "Virgil";
+    $players['player'][1]['name'] = "Ye";
+    $players['player'][2]['name'] = "Heron";
+    $players['player'][3]['name'] = "Osiris";
     
-    for($i =0;$i<4;$i++){
-        $players['player'][$i]['hand'] = array_pop($deck);
-        }
-        return $players;
+    for($i = 0; $i < 4; $i++){
+        $players['player'][$i]['value'] = 0;
+        $x = 0;
+        do{
+            $players['player'][$i]['hand'][$x] = array_pop($deck['cards']);
+            $players['player'][$i]['value'] = $players['player'][$i]['value'] + $players['player'][$i]['hand'][$x]['face'];
+            $x = $x + 1;
+        } while($players['player'][$i]['value'] < 37);
+        
     }
+    return $players;
+}
     
     
     
--- index.php
+++ index.php
@@ -3,15 +3,48 @@
 <html>
     <head>
         <title> SilverJack </title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
         <style>
         </style>
     </head>
     <body>
+        <h1>
+            "SILVERJACK"
+        </h1>
         <div id="main">
             <?php
+                $count = 1;
                 include 'functionsA.php';
+                $starttime = microtime(true);
                 play();
+                $endtime = microtime(true);
+                $timediff = $endtime - $starttime;
+                echo "<h2 id='output'> Elapsed time: $timediff seconds </h2>";
             ?>
+            <?php session_start()?>
+            <form name="testForm" method="post">
+            <?php 
+                if (empty($_SESSION['cnt'])){
+                    $_SESSION['cnt'] = 0;
+                    $_SESSION['total_time'] = 0;
+                    echo "<h2 id='output'> Average elapsed time: $timediff seconds </h2>";
+                    echo "<h2 id='output'> Games played: 1 </h2>";
+                }
+            ?>
+
+            <input type="submit" name="next" id="next" value='"PLAY AGAIN"'/>
+
+            <?php
+                 if(isset($_POST['next'])){
+                    $_SESSION['cnt']++;
+                    $_SESSION['total_time'] = $_SESSION['total_time'] + $timediff;
+                    $count = $_SESSION['cnt'] + 1;
+                    $average = $_SESSION['total_time'] / $count;
+                    echo "<h2 id='output'> Average elapsed time: $average seconds </h2>";
+                    echo "<h2 id='output'> Games played: $count </h2>";
+                }
+            ?>
+            </form>
 
         </div>
         
